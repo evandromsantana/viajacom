307e57d94c3d9d8d9c94922bd88ba4f5b0b113d5
evandro.santana <evandro.santana@sead.pi.gov.br>
2024-09-02 12:58:50 -0300

    style: infinit scroll

--- src/_components/Header.jsx
+++ src/_components/Header.jsx
@@ -1,18 +1,19 @@
 import { UserButton, useUser } from "@clerk/clerk-react";
 import { Button } from "./ui/button";
-
-import { MapPinned, Clock4, Phone } from "lucide-react";
+import { FaMapMarked, FaClock, FaPhone } from "react-icons/fa";
 
 function Header() {
+  // eslint-disable-next-line no-unused-vars
   const { user, isSignedIn } = useUser();
+
   return (
     <>
       <div className="flex justify-between items-center shadow-sm p-2 bg-[#eef0fc] text-gray-400 text-[10px]">
         <div className="flex justify-start gap-10">
           <div className="flex gap-4  ">
-            <MapPinned className="h-[16px]" />
+            <FaMapMarked className="text-lg" />
             <div className="flex flex-wrap grid">
-              <span className="">Nosso endereço:</span>
+              <span>Nosso endereço:</span>
               <span>Av. Naçoes Unidas | Teresina</span>
             </div>
           </div>
@@ -20,7 +21,7 @@ function Header() {
           <div className="w-[0.05rem] h-8 bg-gray-200" />
 
           <div className="flex gap-4 ">
-            <Clock4 className="h-[16px]" />
+            <FaClock className="text-lg" />
             <div className="flex flex-wrap grid">
               <span className=" ">Horario de trabalho:</span>
               <span>Das 8:00 ás 22:00, sete dias por semana</span>
@@ -29,7 +30,7 @@ function Header() {
         </div>
 
         <div className="flex gap-4">
-          <Phone className="h-[16px]" />
+          <FaPhone className="text-lg" />
           <div className="flex-wrap grid">
             <span>+ 55 (89)9999 999999</span>
             <span>+ 55 (89)9999 999999</span>
